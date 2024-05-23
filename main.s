43d2f4d4dbc0958c97629e89571425e661e32a18
anuyraS <namadaeva@mail.ru>
2024-05-23 09:58:37 +0800

    Update main.s

--- main.s
+++ main.s
@@ -1,82 +1,82 @@
 	.file	"main.c"
-	.text
+	.text                                                **Начало текста программы
 	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
-	.string	"Cannot read all the data"
+	.string	"Cannot read all the data"                   **Сообщение об ошибке чтения данных
 	.section	.rodata.str1.8,"aMS",@progbits,1
 	.align 8
 .LC1:
-	.string	"(SUBPROCESS) Fibonacci number %ld is %ld\n"
+	.string	"(SUBPROCESS) Fibonacci number %ld is %ld\n" **Формат строки для вывода результата
 	.text
 	.p2align 4
-	.globl	run_fib
+	.globl	run_fib                                      **Определеение глобальной функции
 	.type	run_fib, @function
 run_fib:
 .LFB22:
 	.cfi_startproc
-	pushq	%rbx
+	pushq	%rbx                                         **Сохранить регистр rbx
 	.cfi_def_cfa_offset 16
 	.cfi_offset 3, -16
-	subq	$32, %rsp
+	subq	$32, %rsp                                    **Выделить место в стеке
 	.cfi_def_cfa_offset 48
-	movl	4+p(%rip), %edi
+	movl	4+p(%rip), %edi                              **Загрузить дескриптор файла
 	movq	%fs:40, %rax
 	movq	%rax, 24(%rsp)
 	xorl	%eax, %eax
 	movq	$0, 8(%rsp)
-	call	close@PLT
+	call	close@PLT                                    **Закрыть файл
 	movl	p(%rip), %edi
 	leaq	8(%rsp), %rsi
 	movl	$8, %edx
-	call	read@PLT
+	call	read@PLT                                     **Читать из файла
 	movl	p(%rip), %edi
 	movq	%rax, %rbx
-	call	close@PLT
-	cmpq	$8, %rbx
-	jne	.L6
+	call	close@PLT                                    **Закрыть файл
+	cmpq	$8, %rbx  
+	jne	.L6                                          **Если прочитано не 8 байт, перейти к ошибке
 	movq	8(%rsp), %rdi
-	call	fibonacci@PLT
+	call	fibonacci@PLT                                **Вычислить число Фибоначчи
 	movq	8(%rsp), %rsi
 	leaq	.LC1(%rip), %rdi
 	movq	%rax, %rdx
 	movq	%rax, 16(%rsp)
 	xorl	%eax, %eax
-	call	printf@PLT
+	call	printf@PLT                                   **Вывести результат
 	movl	o(%rip), %edi
-	call	close@PLT
+	call	close@PLT                                    **Закрыть файл
 	movl	4+o(%rip), %edi
 	leaq	16(%rsp), %rsi
 	movl	$8, %edx
-	call	write@PLT
+	call	write@PLT                                    **Записать результат
 	movl	4+o(%rip), %edi
-	call	close@PLT
+	call	close@PLT                                    **Закрыть файл
 	movq	24(%rsp), %rax
 	subq	%fs:40, %rax
 	jne	.L7
-	addq	$32, %rsp
+	addq	$32, %rsp                                    **Восстановаить стек
 	.cfi_remember_state
 	.cfi_def_cfa_offset 16
-	popq	%rbx
+	popq	%rbx                                         **Восстановаить rbx
 	.cfi_def_cfa_offset 8
 	ret
 .L6:
 	.cfi_restore_state
 	leaq	.LC0(%rip), %rdi
-	call	puts@PLT
+	call	puts@PLT                                     **Вывести сообщение об ошибке
 	movl	$1, %edi
-	call	_exit@PLT
+	call	_exit@PLT                                    **Завершить процесс
 .L7:
-	call	__stack_chk_fail@PLT
+	call	__stack_chk_fail@PLT                         **Ошибка стека
 	.cfi_endproc
 .LFE22:
 	.size	run_fib, .-run_fib
 	.section	.rodata.str1.1
 .LC2:
-	.string	"Hello, World!"
+	.string	"Hello, World!"                               **Приветсвенное сообщение
 	.section	.rodata.str1.8
 	.align 8
 .LC3:
-	.string	"Fibonacci number %ld is %ld. (Taken from child process)\n"
+	.string	"Fibonacci number %ld is %ld. (Taken from child process)\n" **Формат строки для вывода результата из дочернего процесса
 	.section	.rodata.str1.1
 .LC4:
 	.string	"A Child process killed."
@@ -87,39 +87,39 @@ run_fib:
 main:
 .LFB23:
 	.cfi_startproc
-	pushq	%rbp
+	pushq	%rbp                                          **Сохранить регистр rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	leaq	.LC2(%rip), %rdi
-	pushq	%rbx
+	leaq	.LC2(%rip), %rdi 
+	pushq	%rbx                                          **Сохранить регистр rbx
 	.cfi_def_cfa_offset 24
 	.cfi_offset 3, -24
-	subq	$24, %rsp
+	subq	$24, %rsp                                     **Выделить место в стеке
 	.cfi_def_cfa_offset 48
 	movq	%fs:40, %rax
 	movq	%rax, 8(%rsp)
 	xorl	%eax, %eax
-	call	puts@PLT
+	call	puts@PLT                                      **Вывести приветственное сообщение
 	leaq	p(%rip), %rdi
-	call	pipe@PLT
+	call	pipe@PLT                                      **Создать pipe
 	leaq	o(%rip), %rdi
-	call	pipe@PLT
-	call	fork@PLT
+	call	pipe@PLT                                      **Создать второй pipe
+	call	fork@PLT                                      **Создать дочерний процесс
 	testl	%eax, %eax
-	jne	.L9
+	jne	.L9                                           **Если в родительском процессе, перейти к метке .L9
 	xorl	%eax, %eax
-	call	run_fib
+	call	run_fib                                       **Вызвать функцию вычисления Фибоначчи
 .L10:
 	movq	8(%rsp), %rax
 	subq	%fs:40, %rax
 	jne	.L13
-	addq	$24, %rsp
+	addq	$24, %rsp                                     **Восстановить стек
 	.cfi_remember_state
 	.cfi_def_cfa_offset 24
 	xorl	%eax, %eax
-	popq	%rbx
+	popq	%rbx                                          **Восстановаить rbx
 	.cfi_def_cfa_offset 16
-	popq	%rbp
+	popq	%rbp                                          **Восстановаить rbp
 	.cfi_def_cfa_offset 8
 	ret
 .L9:
@@ -127,35 +127,35 @@ main:
 	movl	p(%rip), %edi
 	movl	%eax, %ebx
 	movq	%rsp, %rbp
-	call	close@PLT
+	call	close@PLT                                      **Закрыть pipe
 	movl	4+p(%rip), %edi
 	movl	$8, %edx
 	leaq	n(%rip), %rsi
-	call	write@PLT
+	call	write@PLT                                      **Записать в pipe
 	movl	4+p(%rip), %edi
-	call	close@PLT
+	call	close@PLT                                      **Закрыть pipe
 	movl	4+o(%rip), %edi
-	call	close@PLT
+	call	close@PLT                                      **Закрыть второй pipe
 	movl	o(%rip), %edi
 	movq	%rbp, %rsi
 	movl	$8, %edx
-	call	read@PLT
+	call	read@PLT                                       **Читать из pipe
 	movl	o(%rip), %edi
-	call	close@PLT
+	call	close@PLT                                      **Закрыть второй pipe
 	movq	(%rsp), %rdx
 	movq	n(%rip), %rsi
 	xorl	%eax, %eax
 	leaq	.LC3(%rip), %rdi
-	call	printf@PLT
+	call	printf@PLT                                     **Вывести результат
 	movl	%ebx, %edi
 	xorl	%edx, %edx
 	movq	%rbp, %rsi
-	call	waitpid@PLT
+	call	waitpid@PLT                                    **Ожидать завершения дочернего процесса
 	leaq	.LC4(%rip), %rdi
 	call	puts@PLT
 	jmp	.L10
 .L13:
-	call	__stack_chk_fail@PLT
+	call	__stack_chk_fail@PLT                           **Ошибка стека
 	.cfi_endproc
 .LFE23:
 	.size	main, .-main
@@ -165,19 +165,19 @@ main:
 	.type	n, @object
 	.size	n, 8
 n:
-	.quad	12
+	.quad	12                                             **Исходное число для вычисления Фибоначчи
 	.globl	o
 	.bss
 	.align 8
 	.type	o, @object
 	.size	o, 8
 o:
-	.zero	8
+	.zero	8                                             **Зарезервированное место
 	.globl	p
 	.align 8
 	.type	p, @object
 	.size	p, 8
 p:
-	.zero	8
+	.zero	8                                             **Зарезервированное место
 	.ident	"GCC: (GNU) 13.2.1 20230801"
 	.section	.note.GNU-stack,"",@progbits
